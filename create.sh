6478384b88ec9d963bae335b6eea91878efee817
nick-sakovich <nick.sakovich@codefresh.io>
2017-11-27 15:42:26 +0300

    fixed issue with request body

--- create.sh
+++ create.sh
@@ -1,1 +1,1 @@
-curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST -d '{"title": "${TITLE}", "base": "${BASE}", "head": "${HEAD}"}' https://api.github.com/repos/${GITHUB_REPO_OWNER}/${GITHUB_REPO_NAME}/pulls
+curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST -d '{"title": "'"${TITLE}"'", "base": "'"${BASE}"'", "head": "'"${HEAD}"'"}' https://api.github.com/repos/${GITHUB_REPO_OWNER}/${GITHUB_REPO_NAME}/pulls
